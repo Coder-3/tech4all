a6b1e7305a735d02257afe9beddd0b8a91e15313
Cédric <cedricquenette@outlook.com>
2022-04-02 14:56:31 +1100

    Added mantine component library and made changes to the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,103 @@
 import Head from "next/head"
 import { attributes, react as HomeContent } from '../content/home.md';
+import {
+  createStyles,
+  MantineProvider,
+  AppShell,
+  Header,
+  Navbar,
+  Burger,
+  MediaQuery,
+  Text,
+  Anchor
+} from '@mantine/core';
+import { NextLink } from '@mantine/next'
+import { useState } from 'react';
+
+const useStyles = createStyles((theme) => ({
+  navbar: {
+    [theme.fn.largerThan("sm")]: {
+      display: "none"
+    }
+  },
+
+  navContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100%',
+    margin: '0 20px'
+  },
+
+  links: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    height: '100%',
+    gap: 20,
+    [theme.fn.smallerThan("sm")]: {
+      display: "none"
+    }
+  }
+}));
 
 export default function Knowledge() {
-  const { title } = attributes
+  const { classes } = useStyles()
+  const [opened, setOpened] = useState(false)
+  const { content, title } = attributes
+
   return (
     <>
       <Head>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        <title>{title}</title>
       </Head>
-      <article>
-        <h1>{title}</h1>
-        <HomeContent />
-      </article>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <AppShell
+          fixed
+          navbarOffsetBreakpoint="sm"
+          header={
+            <Header height={50}>
+              <div className={classes.navContainer}>
+                <div>
+                  <Text variant="h2">techforall</Text>
+                </div>
+                <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                  <Burger
+                    opened={opened}
+                    onClick={() => setOpened((o) => !o)}
+                    size="sm"
+                    mr="xl"
+                  />
+                </MediaQuery>
+                <div className={classes.links}>
+                  <Anchor>Vision</Anchor>
+                  <Anchor>Learn</Anchor>
+                  <Anchor>Blog</Anchor>
+                  <Anchor>About</Anchor>
+                </div>
+              </div>
+            </Header>
+          }
+          navbar={
+            <Navbar
+              className={classes.navbar}
+              width={{ base: "100%", sm: 0 }}
+              hidden={!opened}
+            >
+              <Anchor>Vision</Anchor>
+              <Anchor>Learn</Anchor>
+              <Anchor>Blog</Anchor>
+              <Anchor>About</Anchor>
+            </Navbar>
+          }
+        >
+          <article>
+            <h1>{content[0].heading}</h1>
+            <h2>{content[0].subheading}</h2>
+          </article>
+        </AppShell>
+      </MantineProvider>
     </>
   )
 }
