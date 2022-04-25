1b7584ce3848c9bae53a85866896f021b117b8cc
Cedric <cedric@liquid-state.com>
2022-04-25 13:01:33 +1000

    Added responsive course overview components for the homepage

---
+++ components/Course.js
@@ -0,0 +1,84 @@
+import { Button, createStyles, Image } from "@mantine/core";
+import Link from "next/link";
+
+const lighterOrange = "#E49759";
+const lighterBlue = "#59A6E4";
+const darkerOrange = "#DD7C2D";
+const darkerBlue = "#2D8EDD";
+const darkestBlue = "#1E73B9";
+
+const useStyles = createStyles((theme) => ({
+  courseContainer: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    maxWidth: "33%",
+    borderRadius: "24px",
+    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+
+    img: {
+      borderRadius: "24px 24px 0 0",
+    },
+
+    [theme.fn.smallerThan("md")]: {
+      maxWidth: "80%",
+      margin: "20px auto",
+    },
+  },
+
+  row: {
+    margin: "20px",
+  },
+
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "center",
+  },
+
+  learnMoreButton: {
+    width: "90%",
+    height: "auto",
+    minHeight: "30px",
+    color: "white",
+    border: 0,
+    borderRadius: "12px",
+    letterSpacing: "2px",
+    textTransform: "uppercase",
+    padding: "15px 30px",
+    backgroundColor: darkerBlue,
+    marginTop: "15px",
+
+    "&:hover": {
+      backgroundColor: darkestBlue,
+    },
+  },
+}));
+
+const Course = ({ image, name, description, skills, link }) => {
+  const { classes } = useStyles();
+
+  return (
+    <div className={classes.courseContainer}>
+      <Image src={image} />
+      <div className={classes.row}>
+        <h3>{name}</h3>
+        <p>{description}</p>
+      </div>
+      <div className={classes.row}>
+        <h3>Skills</h3>
+        <ul>
+          {skills.map((skill) => (
+            <li key={skill}>{skill}</li>
+          ))}
+        </ul>
+      </div>
+      <div className={`${classes.row} ${classes.buttonContainer}`}>
+        <Button className={classes.learnMoreButton} variant="primary">
+          Learn More
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Course;
