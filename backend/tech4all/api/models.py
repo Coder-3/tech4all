ba4c5101459fd310491586fad49d09ff8e23a3ac
Cedric <cedric@liquid-state.com>
2022-05-30 08:14:12 +1000

    Initial work on the models

--- backend/tech4all/api/models.py
+++ backend/tech4all/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from model_utils.fields import StatusField
+from model_utils import Choices
 
-# Create your models here.
+
+class Track(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Module(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    track = models.ForeignKey(Track, on_delete=models.CASCADE)
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    link = models.URLField()
+    is_completed = models.BooleanField(default=False)
+    is_started = models.BooleanField(default=False)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+
