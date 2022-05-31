eb5b1611521a4b97f6726afe50e229afdecc1802
Cedric <cedric@liquid-state.com>
2022-06-01 07:28:09 +1000

    Moved the app shell into the app component

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,105 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { Global, MantineProvider } from "@mantine/core";
-import Navigation from "../components/Navigation";
+import {
+  AppShell,
+  Burger,
+  Button,
+  Global,
+  Header,
+  MantineProvider,
+  MediaQuery,
+  Navbar,
+  Text,
+  createStyles,
+} from "@mantine/core";
+import Link from "next/link";
+import { useState } from "react";
+
+const useStyles = createStyles((theme) => ({
+  navBurger: {
+    display: "flex",
+    alignItems: "center",
+    paddingTop: "30px",
+    [theme.fn.largerThan("md")]: {
+      display: "none",
+    },
+
+    a: {
+      color: "black",
+      fontSize: "1.2rem",
+      textDecoration: "none",
+      margin: "0 10px",
+    },
+  },
+
+  navContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "100%",
+    maxWidth: "1200px",
+    marginLeft: "auto",
+    marginRight: "auto",
+
+    a: {
+      color: "black",
+      fontSize: "1.2rem",
+      textDecoration: "none",
+
+      "&:hover": {
+        color: "#2D8EDD",
+      },
+    },
+
+    [theme.fn.smallerThan("lg")]: {
+      justifyContent: "space-between",
+      marginLeft: "24px",
+      marginRight: "24px",
+    },
+  },
+
+  links: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    gap: 50,
+    [theme.fn.smallerThan("md")]: {
+      display: "none",
+    },
+  },
+
+  joinUsButton: {
+    width: "200px",
+    height: "auto",
+    minHeight: "30px",
+    color: "white",
+    border: 0,
+    borderRadius: "12px",
+    letterSpacing: "2px",
+    textTransform: "uppercase",
+    padding: "15px 30px",
+  },
+
+  joinUsButtonDesktop: {
+    [theme.fn.smallerThan("md")]: {
+      display: "none",
+    },
+  },
+
+  joinUsButtonBurger: {
+    marginBottom: "15px",
+  },
+
+  logo: {
+    width: "200px",
+  },
+}));
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const { classes } = useStyles();
+  const [opened, setOpened] = useState(false);
+
   return (
     <>
       <Head>
@@ -103,9 +199,57 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           primaryColor: "dark-blue",
         }}
       >
-        <Navigation>
+        <AppShell
+          fixed
+          navbarOffsetBreakpoint="sm"
+          header={
+            <Header height={100}>
+              <div className={classes.navContainer}>
+                <div className={classes.logo}>
+                  <Text>Tech4All</Text>
+                </div>
+                <MediaQuery largerThan="md" styles={{ display: "none" }}>
+                  <Burger
+                    opened={opened}
+                    onClick={() => setOpened((o) => !o)}
+                    size="md"
+                  />
+                </MediaQuery>
+                <div className={classes.links}>
+                  <Link href="/vision">Vision</Link>
+                  <Link href="/learn">Learn</Link>
+                  <Link href="/blog">Blog</Link>
+                  <Link href="/about">About</Link>
+                </div>
+                <div className={classes.joinUsButtonDesktop}>
+                  <Button color="dark-blue" className={classes.joinUsButton}>
+                    Join Us
+                  </Button>
+                </div>
+              </div>
+            </Header>
+          }
+          navbar={
+            <Navbar
+              className={classes.navBurger}
+              width={{ base: "100%", md: 0 }}
+              hidden={!opened}
+            >
+              <Button
+                color="dark-blue"
+                className={`${classes.joinUsButton} ${classes.joinUsButtonBurger}`}
+              >
+                Join Us
+              </Button>
+              <Link href="/vision">Vision</Link>
+              <Link href="/learn">Learn</Link>
+              <Link href="/blog">Blog</Link>
+              <Link href="/about">About</Link>
+            </Navbar>
+          }
+        >
           <Component {...pageProps} />
-        </Navigation>
+        </AppShell>
       </MantineProvider>
     </>
   );
