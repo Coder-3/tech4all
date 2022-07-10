08ec6a9422dca97aee3708055876e5925324ac57
Amber Jain <amberjain1234@gmail.com>
2022-07-10 20:41:11 +1000

    Added Accordian

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,4 @@
-import {
-  Container,
-  Timeline,
-  Avatar
-} from "@mantine/core";
+import { Container, Timeline, Avatar, Accordion } from "@mantine/core";
 import { User } from "@supabase/supabase-js";
 import type { NextPage } from "next";
 import Head from "next/head";
@@ -15,17 +11,30 @@ const Dashboard: NextPage = ({}) => {
   const [lessonsUsers, setLessonsUsers] = useState([]);
   const [userState, setUserState] = useState<User | null>(null);
   const [lessons, setLessons] = useState<Array<any>>();
-
+  const [tracks, setTracks] = useState([]);
 
   const user = supabase.auth.user();
   useEffect(() => {
     setUserState(supabase.auth.user());
   }, [user]);
 
+  useEffect(() => {
+      const getTracks = async () => {
+          const {data: tracks} = await supabase
+          .from("tracks")
+          .select("*")
+          return tracks
+      }
+      getTracks().then((res: any) => {
+        setTracks(res);
+      });
+  }, []);
 
   useEffect(() => {
     const getMappedUserLessons = async () => {
-      const { data: lessons } = await supabase.from("lessons").select('id, title');
+      const { data: lessons } = await supabase
+        .from("lessons")
+        .select("id, title");
       const { data: lessonsUsers } = await supabase
         .from("lessons_users")
         .select("*");
@@ -37,7 +46,7 @@ const Dashboard: NextPage = ({}) => {
               lessonUser.lesson_id === lesson.id &&
               lessonUser.user_id === userState?.id &&
               lessonUser.is_completed
-          )
+          ),
         };
       });
       return formattedLessons;
@@ -53,32 +62,41 @@ const Dashboard: NextPage = ({}) => {
         <Head>
           <title>Dashboard</title>
         </Head>
-        <div style={{ maxWidth: 320, margin: "auto" }}>
-          <Timeline>
-            {lessons?.sort((a, b) => a.id - b.id).map((lesson) =>
-              lesson.is_completed ? (
-                <Timeline.Item
-                  key={`lesson-${lesson.id}`}
-                  title={lesson.title}
-                  bulletSize={24}
-                  active
-                  bullet={<Avatar
-                    size={22}
-                    radius="xl"
-                    src="images/icon color.png"
-                  />}
-                >
-                </Timeline.Item>
-              ) : (
-                <Timeline.Item
-                  key={`lesson-${lesson.id}`}
-                  title={lesson.title}
-                  bulletSize={24}
-                ></Timeline.Item>
-              )
+        <Accordion initialItem={2}>
+            {
+            tracks?.map((track: any) => <Accordion.Item key={`track-${track.id}`} label={track.title}>
+            <div style={{ maxWidth: 320, margin: "auto" }}>
+              <Timeline>
+                {lessons
+                  ?.sort((a, b) => a.id - b.id)
+                  .map((lesson) =>
+                    lesson.is_completed ? (
+                      <Timeline.Item
+                        key={`lesson-${lesson.id}`}
+                        title={lesson.title}
+                        bulletSize={24}
+                        active
+                        bullet={
+                          <Avatar
+                            size={22}
+                            radius="xl"
+                            src="images/icon color.png"
+                          />
+                        }
+                      ></Timeline.Item>
+                    ) : (
+                      <Timeline.Item
+                        key={`lesson-${lesson.id}`}
+                        title={lesson.title}
+                        bulletSize={24}
+                      ></Timeline.Item>
+                    )
+                  )}
+              </Timeline>
+            </div>
+            </Accordion.Item>
             )}
-          </Timeline>
-        </div>
+        </Accordion>
       </Container>
     </>
   );
